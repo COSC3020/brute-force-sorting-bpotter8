29b2e25fcb32942f2970a85b3d9c06da03824cb2
Braden Potter <144744976+bpotter8@users.noreply.github.com>
2024-04-02 21:55:00 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,35 +1,38 @@
-function permutationSort(array) {
+function permutationSort(a) {
     function sorted(arr) {
-        for (let i = 0; i < arr.length - 1; i++) {
+        for (var i = 0; i < arr.length - 1; i++) {
             if (arr[i] > arr[i + 1]) {
                 return false;
             }
         }
         return true;
     }
-
+    
     function swap(arr, i, j) {
         const x = arr[i];
         arr[i] = arr[j];
         arr[j] = x;
     }
-
-    var perms = 0;
+    
     function permute(arr, leftInd) {
-        if (leftInd === arr.length - 1) {
-            perms++;
-            if (sorted(arr)) {
-                return arr;
+        var perms = 0;
+        const l = leftInd + 1;
+        if(arr.length <= l){
+            return 1;
+        }
+        for (var i = leftInd; i < arr.length; i++) {
+            if (arr[leftInd] != arr[i]){
+                swap(arr, leftInd, i);
+            }
+            perms += permute(arr, l);
+            if (sorted(arr)){
+                return perms;
+            } else if (arr[leftInd] != arr[i]){
+                swap (arr, leftInd, i);
             }
         }
-
-        for (let i = leftInd; i < arr.length; i++) {
-            swap(arr, leftInd, i);
-            permute(arr, leftInd + 1);
-            swap(arr, leftInd, i);
-        }
+        return perms;
     }
-
-    permute(array, 0);
-    return perms;
+   
+    return permute(a, 0);
 }
