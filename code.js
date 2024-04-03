bd168d20ff274e9306f255d265176ffc903545f4
Braden Potter <144744976+bpotter8@users.noreply.github.com>
2024-04-02 20:56:51 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,35 @@
-function permutationSort(a) {
-    return 0;
+function permutationSort(arr) {
+    function sorted(arr) {
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function swap(arr, i, j) {
+        const x = arr[i];
+        arr[i] = arr[j];
+        arr[j] = x;
+    }
+
+    var perms = 0;
+    function permute(arr, leftInd) {
+        if (leftInd === arr.length - 1) {
+            perms++;
+            if (sorted(arr)) {
+                return;
+            }
+        }
+
+        for (let i = leftInd; i < arr.length; i++) {
+            swap(arr, leftInd, i);
+            permute(arr, leftInd + 1);
+            swap(arr, leftInd, i);
+        }
+    }
+
+    permute(arr, 0);
+    return perms;
 }
